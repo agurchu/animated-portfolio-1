3d905b2d2ff9075da9592b086d9b520db1357edb
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-01 22:56:08 +0200

    js script

---
+++ script.js
@@ -0,0 +1,7 @@
+const typingEffect = new Typed(".multiText", {
+  strings: ["Coder", "web developer", "designer", "music producer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1500,
+});
