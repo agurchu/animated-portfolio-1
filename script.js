6b31550b902bc303f9c08efb65a6f9acdfaef91f
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-02 07:32:56 +0200

    nav media adjustments

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const typingEffect = new Typed(".multiText", {
   backSpeed: 80,
   backDelay: 1500,
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navItems = document.querySelector(".nav-items");
+
+hamburger.addEventListener("click", () => {
+  navItems.classList.toggle("open");
+});
